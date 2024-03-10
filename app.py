d7acc1c8730859bbace6d1fc80971489822d6854
Nabdeep Patel <132994846+nabdeep-patel@users.noreply.github.com>
2024-03-10 22:21:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,14 @@ def main():
     st.sidebar.markdown("- [app.py](#app)")
     st.sidebar.markdown("- [GitHub](#github)")
     st.sidebar.markdown("- [IPython Notebook](#ipython-notebook)")
+    st.write("---")
+    st.markdown(
+        "[![LinkedIn](https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/nabdeeppatel)"
+    )
+    st.markdown(
+        "[![GitHub](https://img.shields.io/badge/GitHub-181717?style=for-the-badge&logo=github&logoColor=white)](https://github.com/nabdeep-patel)
+"
+    )
 
     # Anchor tags for navigation
     st.markdown("<a id='app'></a>", unsafe_allow_html=True)
