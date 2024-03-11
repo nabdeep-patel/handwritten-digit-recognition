d49029efa8ab03423f09609bc4b888499a2e27ec
Nabdeep Patel <132994846+nabdeep-patel@users.noreply.github.com>
2024-03-11 20:04:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,10 +15,10 @@ def main():
     github_link = "[![GitHub](https://img.shields.io/badge/GitHub-Profile-blue?style=flat-square&logo=github)](https://github.com/nabdeep-patel)"
     linkedin_link = "[![LinkedIn](https://img.shields.io/badge/LinkedIn-Profile-blue?style=flat-square&logo=linkedin)](https://www.linkedin.com/in/nabdeeppatel)"
     website_link = "[![Website](https://img.shields.io/badge/Personal-Website-blue?style=flat-square&logo=chrome)](https://linktr.ee/nabdeeppatel/store)"
-    email_link = "[![Email](https://img.shields.io/badge/Email-nabdeeppatel%40gmail.com-blue?style=flat-square&logo=gmail)](mailto:nabdeeppatel@gmail.com)"
+    email_link = "[![Email](https://img.shields.io/badge/Google-Mail-blue?style=flat-square&logo=gmail)](mailto:nabdeeppatel@gmail.com)"
     
     st.sidebar.markdown(github_link + " " + linkedin_link + " " + website_link + " " + email_link)
-    
+    st.sidebar.markdown("Created by Nabdeep Patel")
     
     # GitHub badge
     github_badge = "[![GitHub](https://img.shields.io/badge/GitHub-181717?style=for-the-badge&logo=github&logoColor=white)](https://github.com/nabdeep-patel)"
