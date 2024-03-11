19248bc60dd87abbd74c68b9fe9354915f53f9ed
Nabdeep Patel <132994846+nabdeep-patel@users.noreply.github.com>
2024-03-11 19:52:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,13 +10,14 @@ def main():
     st.sidebar.markdown("- [IPython Notebook](#ipython-notebook)")
     st.write("---")
     st.sidebar.markdown(
-        "Connect with me:"
-[![GitHub](https://img.shields.io/badge/GitHub-Profile-blue?style=flat-square&logo=github)](https://github.com/nabdeep-patel)
-[![LinkedIn](https://img.shields.io/badge/LinkedIn-Profile-blue?style=flat-square&logo=linkedin)](https://www.linkedin.com/in/nabdeeppatel)
-[![Website](https://img.shields.io/badge/Personal-Website-blue?style=flat-square&logo=chrome)](https://linktr.ee/nabdeeppatel/store)
-
+        "Connect with me:\n"
+        "[![GitHub](https://img.shields.io/badge/GitHub-Profile-blue?style=flat-square&logo=github)](https://github.com/nabdeep-patel)\n"
+        "[![LinkedIn](https://img.shields.io/badge/LinkedIn-Profile-blue?style=flat-square&logo=linkedin)](https://www.linkedin.com/in/nabdeeppatel)\n"
+        "[![Website](https://img.shields.io/badge/Personal-Website-blue?style=flat-square&logo=chrome)](https://linktr.ee/nabdeeppatel/store)"
     )
     st.markdown(
-        "[![GitHub](https://img.shields.io/badge/GitHub-181717?style=for-the-badge&logo=github&logoColor=white)](https://github.com/nabdeep-patel)
-"
+        "[![GitHub](https://img.shields.io/badge/GitHub-181717?style=for-the-badge&logo=github&logoColor=white)](https://github.com/nabdeep-patel)"
     )
+
+if __name__ == "__main__":
+    main()
