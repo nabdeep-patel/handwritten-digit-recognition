99b5c19f792a55ac25b450cb0b78bb1ec88916c8
Nabdeep Patel <132994846+nabdeep-patel@users.noreply.github.com>
2024-03-11 19:59:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,16 +8,20 @@ def main():
     st.sidebar.markdown("- [app.py](#app)")
     st.sidebar.markdown("- [GitHub](#github)")
     st.sidebar.markdown("- [IPython Notebook](#ipython-notebook)")
-    st.write("---")
+    st.sidebar.write("---")
     
     # Connect with me section
     st.sidebar.markdown("Connect with me:")
     
+st.sidebar.markdown("Connect with me:")
+    
     github_link = "[![GitHub](https://img.shields.io/badge/GitHub-Profile-blue?style=flat-square&logo=github)](https://github.com/nabdeep-patel)"
     linkedin_link = "[![LinkedIn](https://img.shields.io/badge/LinkedIn-Profile-blue?style=flat-square&logo=linkedin)](https://www.linkedin.com/in/nabdeeppatel)"
     website_link = "[![Website](https://img.shields.io/badge/Personal-Website-blue?style=flat-square&logo=chrome)](https://linktr.ee/nabdeeppatel/store)"
+    email_link = "[![Email](https://img.shields.io/badge/Email-nabdeeppatel%40gmail.com-blue?style=flat-square&logo=gmail)](mailto:nabdeeppatel@gmail.com)"
     
-    st.sidebar.markdown(github_link + " " + linkedin_link + " " + website_link)
+    st.sidebar.markdown(github_link + " " + linkedin_link + " " + website_link + " " + email_link)
+    
     
     # GitHub badge
     github_badge = "[![GitHub](https://img.shields.io/badge/GitHub-181717?style=for-the-badge&logo=github&logoColor=white)](https://github.com/nabdeep-patel)"
