83a0bc767e62cf451ee402a429dd1f420f1c5e49
Nabdeep Patel <132994846+nabdeep-patel@users.noreply.github.com>
2024-03-12 18:37:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -52,8 +52,9 @@ def mycanvas():
     
     st.write("Image of the canvas")
     if canvas_result.image_data is not None:
+        st.image(canvas_result.image_data)
         preprocessed_image = preprocess_image(canvas_result.image_data)
-        st.image(preprocessed_image)
+        
         st.write("Preprocessed Image Shape:", preprocessed_image.shape)
 
 if __name__ == "__main__":
