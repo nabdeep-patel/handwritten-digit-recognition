0ef28c2ba888ee3b8b90e41c7f89b53671c9c1ec
Nabdeep Patel <132994846+nabdeep-patel@users.noreply.github.com>
2024-03-12 19:05:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,33 @@
 import streamlit as st
+from PIL import Image
 from streamlit_drawable_canvas import st_canvas
 import numpy as np
-from PIL import Image
+import tensorflow as tf
+import gdown
+
+def preprocess_image(image_data):
+    """
+    Preprocesses the image data obtained from the canvas.
+
+    Parameters:
+        image_data (numpy.ndarray): The image data obtained from the canvas.
+
+    Returns:
+        numpy.ndarray: The preprocessed image data.
+    """
+    # Convert image data to PIL Image
+    image = Image.fromarray(image_data)
+
+    # Resize the image to (100, 100) for downsampling
+    resized_image = image.resize((100, 100))
+
+    # Convert resized image to numpy array
+    resized_image_data = np.array(resized_image)
+
+    # Flatten the resized image data
+    flattened_image_data = resized_image_data.reshape(10000, 4)
+
+    return flattened_image_data
 
 def main():
     st.title("Simple Streamlit App")
@@ -23,18 +49,6 @@ def main():
     st.sidebar.markdown(github_link + " " + linkedin_link + " " + website_link + " " + email_link)
     st.sidebar.markdown("Created by Nabdeep Patel")
     mycanvas()
-def preprocess_image(image_data):
-    image = Image.fromarray(image_data)
-
-    # Resize the image to (100, 100) for downsampling
-    resized_image = image.resize((100, 100))
-
-    # Convert resized image to numpy array
-    resized_image_data = np.array(resized_image)
-
-    # Flatten the resized image data
-    flattened_image_data = resized_image_data.reshape(10000, 4)
-    return flattened_image_data
 
 def mycanvas():
     st.write("Canvas")
@@ -54,8 +68,22 @@ def mycanvas():
     if canvas_result.image_data is not None:
         st.image(canvas_result.image_data)
         preprocessed_image = preprocess_image(canvas_result.image_data)
-        
         st.write("Preprocessed Image Shape:", preprocessed_image.shape)
+
+        # Download the model file from Google Drive
+        url = 'https://docs.google.com/uc?export=download&id=16m69DmL-r-x2bBNhKuyheDoUtFWxBFcq'
+        output = 'model.h5'
+        gdown.download(url, output, quiet=False)
+
+        # Load the model
+        model = tf.keras.models.load_model('model.h5')
+
+        # Make predictions
+        predicted_probs = model.predict(preprocessed_image.reshape(1, 10000, 4))
+        predicted_classes = np.argmax(predicted_probs, axis=1)
+
+        # Display predicted classes
+        st.write("Predicted Classes:", predicted_classes)
 
 if __name__ == "__main__":
     main()
