ee9e66125b4d3540b41d6e7979866e724429661c
Nabdeep Patel <132994846+nabdeep-patel@users.noreply.github.com>
2024-03-14 22:40:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,18 +4,6 @@ from streamlit_drawable_canvas import st_canvas
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
-import gdown
-
-def download_model():
-    url = 'https://drive.google.com/uc?id=16m69DmL-r-x2bBNhKuyheDoUtFWxBFcq'
-    output = 'model.h5'
-    gdown.download(url, output, quiet=False)
-
-def load_model():
-    if not os.path.exists('model.h5'):
-        download_model()
-    model = tf.keras.models.load_model('model.h5')
-    return model
 
 def preprocess_image(image_data):
     image = Image.fromarray(image_data)
@@ -95,7 +83,7 @@ def mycanvas():
         
         if st.button("Get Prediction"):
             st.pyplot(fig)
-            model = load_model()
+            model = tf.keras.models.load_model('https://drive.google.com/uc?export=download&id=16m69DmL-r-x2bBNhKuyheDoUtFWxBFcq')
             predictions = model.predict(preprocessed_image_vector)
             # Print the predictions
             st.write("Predictions:")
